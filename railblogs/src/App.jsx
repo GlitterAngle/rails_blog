221ffe582da043318521f1ba2d8e07a02caab0b4
cd <lesleyrotonto@gmail.com>
2024-10-02 19:05:45 -0400

    basic get of post titles

--- railblogs/src/App.jsx
+++ railblogs/src/App.jsx
@@ -6,13 +6,19 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 
 const API_USER_DATA = "http://localhost:3000/api/v1/users"
+const API_POST_DATA = "http://localhost:3000/api/v1/posts"
 
 async function getUserDatat (){
   return await axios.get(API_USER_DATA).then((response)=>response.data)
 }
 
+async function getPostData(){
+  return await axios.get(API_POST_DATA).then((response)=>response.data)
+}
+
 function App() {
   const [users, setUsers] = useState([])
+  const [posts, setposts] = useState([])
 
   useEffect(()=>{
     let mounted = true
@@ -24,13 +30,22 @@ function App() {
     return ()=> mounted=false
   },[])
 
+  useEffect(()=>{
+    let mounted = true
+    getPostData().then((items)=>{
+      if(mounted){
+        setposts(items)
+      }
+    })
+  },[])
+
   return (
     <>
       <Nav/>
       <Routes>
         <Route 
         path='/'
-        element={<Homepage users={users}/>}
+        element={<Homepage users={users} posts={posts}/>}
         />
       </Routes>
     </>
--- railblogs/src/pages/Homepage.jsx
+++ railblogs/src/pages/Homepage.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 
 const Homepage = (props) => {
-  return (
-    <div>
-        <h1>Users Printed from API</h1>
-        {props.users.map((user)=>{
-            return <div key={user.id}>
-                <h2>{user.username}</h2>
-            </div>
-        })}
-    </div>
-  )
+    return (
+        <div>
+            <h1>Users Printed from API</h1>
+            {props.users.map((user) => {
+                return <div key={user.id}>
+                    <h2>{user.username}</h2>
+                    {props.posts
+                    .filter((post) => post.user_id === user.id)
+                        .map((post) => (
+                            <div key={post.id}>
+                                <h4>Title: {post.title}</h4>
+                            </div>
+                        ))
+                    }
+                </div>
+            })}
+        </div>
+    )
 }
 
 export default Homepage
